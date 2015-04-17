116dac2f3f6027b8e3c55472e67ae384a3e00d51
shotakyumon <shotakyumon@gmail.com>
2015-04-18 00:14:51 +0900

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Decreasing the cost factor of bcrypt' raises the speed of test.
+  ActiveModel::SecurePassword.min_cost = true
 end
